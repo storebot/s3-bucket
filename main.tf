9fe3a9cc9535e00a19247a24d2c1131235b094a5
Accurics <admin@accurics.com>
2020-10-14 22:33:32 +0000

    Auto Remediation By Accurics

--- main.tf
+++ main.tf
@@ -12,3 +12,29 @@ resource "aws_s3_bucket" "b" {
     Name = var.bucket_name
   }
 }
+
+resource "aws_s3_bucket_policy" "b" {
+  bucket = "${aws_s3_bucket.b.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "b-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.b.id}/*"
+    }
+  ]
+}
+POLICY
+}

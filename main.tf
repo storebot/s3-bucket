ed94d2bbf74b53bb4c98dd5b4677bad784c81420
Cesar Rodriguez <cesar@accurics.com>
2020-10-14 14:21:59 -0400

    adds s3 bucket terraform

---
+++ main.tf
@@ -0,0 +1,14 @@
+### S3 Bucket placed behind CloudFront
+variable "bucket_name" {}
+resource "aws_s3_bucket" "b" {
+  bucket = var.bucket_name
+  acl    = "public"
+
+  #  versioning {
+  #    enabled = true
+  #  }
+
+  tags = {
+    Name = var.bucket_name
+  }
+}
